9046e3e2f11f86a0ac65911732977aa58f94ad5c
XK <47260768+XK9274@users.noreply.github.com>
2023-07-18 21:54:36 +0100

    Update launch.sh

--- Screencap Toolkit/App/ScreenRecorder/launch.sh
+++ Screencap Toolkit/App/ScreenRecorder/launch.sh
@@ -6,6 +6,7 @@ export sysdir=/mnt/SDCARD/.tmp_update
 export miyoodir=/mnt/SDCARD/miyoo
 export LD_LIBRARY_PATH="$libdir:/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"
 export PATH="$sysdir/bin:$PATH"
+mkdir output
 
 if pgrep "ffmpeg" > /dev/null; then
     killall -2 ffmpeg
